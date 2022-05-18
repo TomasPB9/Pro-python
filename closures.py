00c6514aef475422767c171719cc76230f08774b
Carlos Bolio <tomas.pbolio@gmail.com>
2022-05-18 17:54:58 -0500

    add to closures  file a challlenge  in py

--- closures.py
+++ closures.py
@@ -12,9 +12,27 @@ def make_repeater_of(n):
     return repeater
 
 
+#challenge
+def make_division_by(num):
+    def division_by(divisor):
+        return divisor/num
+    return division_by
+
+
 def run():
     repeat_5 = make_repeater_of(5)
     print(repeat_5("Hola"))
+
+    division_by_3 = make_division_by(3)
+    print(division_by_3(18))
+
+    division_by_5 = make_division_by(5)
+    print(division_by_5(100))
+
+    division_by_18 = make_division_by(18)
+    print(division_by_18(54))
+
+
 
 if __name__ == '__main__':
     run()
