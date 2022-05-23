1631bf4106dd38a59ecaa2b2e9b8d68901c077a6
Carlos Bolio <tomas.pbolio@gmail.com>
2022-05-23 12:41:32 -0500

    adding the challenge a limit succesion fibonacci with generator

--- generator_fibonacci.py
+++ generator_fibonacci.py
@@ -1,7 +1,7 @@
 import time
 
 #creacion del generador
-def fibo_gen():
+def fibo_gen(max: int):
     n1 = 0
     n2 = 1
     counter = 0
@@ -13,17 +13,20 @@ def fibo_gen():
         elif counter == 1:
             counter += 1
             yield n2   #devuelve 1
-        else:
+        elif not max or counter <= max:
             aux = n1 + n2
             #swapping
             n1 , n2 = n2 , aux
             counter += 1
             yield aux   #devuelve los demas elementos de la suscesion fibonacci
+        else:
+            #sucesion fibonacci finita
+            break
 
 
 if __name__=='__main__':
     #instancia del generador (fibogen retorna un generador) se guarda en la variable "fibonacci"  
-    fibonacci = fibo_gen()
+    fibonacci = fibo_gen(5)
     #recorrer cada uno de los elementos del generador
     for element in fibonacci:
         print(element)
